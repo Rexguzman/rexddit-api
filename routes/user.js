fbfef7d4a4592dfc1b332d101f502c5f0759241c
rex guzman <rexguzman1@gmail.com>
2021-03-08 23:22:20 -0400

    delete

--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,7 @@ const express = require('express');
 
 const UsersService = require('../services/users');
 const validationHandler = require('../utils/middleware/validationHandler');
-const scopesValidationHandler = require('../utils/middleware/scopesValidationHandler');
 
-const {
-    userToDoIdSchema,
-    createUserToDoSchema,
-    updateUserToDoSchema,
-    completedUserToDoSchema,
-} = require('../utils/schemas/userToDos');
-const { ObjectId } = require('bson');
 
 function userApi(app) {
     const router = express.Router();
